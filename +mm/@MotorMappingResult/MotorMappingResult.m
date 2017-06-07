d43b78d52bf8a85b7a2bece083736688fbc7d21b
johnmbarrett <john.barrett@cantab.net>
2017-06-07 13:49:09 -0500

    Abstract out getters and setters from MotorMappingResult

--- +mm/@MotorMappingResult/MotorMappingResult.m
+++ +mm/@MotorMappingResult/MotorMappingResult.m
@@ -1,22 +1,36 @@
-classdef MotorMappingResult
+classdef MotorMappingResult < handle % MATLAB you are so dumb
     properties(Access=public)
         AlignmentInfo
     end
     
     properties(Access=public,Dependent=true)
         BodyParts
+        Map % TODO : should this be a stored property?  it can be calculated from Trajectories, also you can imagine many different maps of different movement parameters.  then again, there's an argument to be made for at least caching it for speed
+        MotionTubes
+        PathLengths % TODO : again, this can be computed from Trajectories
+        ROIs
+        Trajectories
     end
     
     properties(Access=protected)
         BodyParts_
     end
     
-    properties(GetAccess=public,SetAccess=protected)
-        Map % TODO : should this be a stored property?  it can be calculated from Trajectories, also you can imagine many different maps of different movement parameters.  then again, there's an argument to be made for at least caching it for speed
-        MotionTubes
-        PathLengths % TODO : again, this can be computed from Trajectories
-        ROIs
-        Trajectories
+    methods(Abstract=true,Access=protected) % MATLAB why
+        map = getMap(self);
+        setMap(self,map);
+        
+        mt = getMotionTubes(self);
+        setMotionTubes(self,mt);
+        
+        pl = getPathLengths(self);
+        setPathLengths(self,pl);
+        
+        rois = getROIs(self);
+        setROIs(self,rois);
+        
+        trajectories = getTrajectories(self);
+        setTrajectories(self,trajectories);
     end
     
     methods
@@ -37,7 +51,7 @@ classdef MotorMappingResult
             la = self.AlignmentInfo;
         end
         
-        function self = set.AlignmentInfo(self,la)
+        function set.AlignmentInfo(self,la)
             assert(isa(la,'mm.LaserAlignment') || (isnumeric(la) && isscalar(la) && isnan(la)),'MotorMapping:MotorMappingResult:IllegalLaserAlignment','Alignment info must be of class mm.LaserAlignment or a scalar NaN');
             self.AlignmentInfo = la;
         end
@@ -46,10 +60,51 @@ classdef MotorMappingResult
             bodyParts = self.BodyParts_;
         end
         
-        function self = set.BodyParts(self,bodyParts)
+        function set.BodyParts(self,bodyParts)
             assert(iscellstr(bodyParts) && numel(bodyParts) == size(self.Map,2),'MotorMapping:MotorMappingResult:InvalidBodyParts','BodyParts must be a cell array of strings of length equal to the number of columns in Map');
             
             self.BodyParts_ = bodyParts;
+        end
+        
+        % fuckingh let me make these abstract you fucking fuck matlabn
+        function map = get.Map(self)
+            map = self.getMap();
+        end
+        
+        function set.Map(self,map)
+            self.setMap(map);
+        end
+        
+        function mt = get.MotionTubes(self)
+            mt = self.getMotionTubes();
+        end
+        
+        function set.MotionTubes(self,mt)
+            self.setMotionTubes(mt);
+        end
+        
+        function pl = get.PathLengths(self)
+            pl = self.getPathLengths();
+        end
+        
+        function set.PathLengths(self,pl)
+            self.setPathLengths(pl);
+        end
+        
+        function rois = get.ROIs(self)
+            rois = self.getROIs();
+        end
+        
+        function set.ROIs(self,rois)
+            self.setROIs(rois);
+        end
+        
+        function trajectories = get.Trajectories(self)
+            trajectories = self.getTrajectories();
+        end
+        
+        function set.Trajectories(self,trajectories)
+            self.setTrajectories(trajectories);
         end
         
         [figs,tf,warpedMaps,refs] = alignHeatmapToBrainImage(map,brainImage,gridParams)
@@ -65,7 +120,7 @@ classdef MotorMappingResult
         hs = plotTrajectories(self,l,varargin)
     end
     
-    methods(Static=true)
+    methods(Static=true) % TODO : should these go in the abstract superclass now?
         function mmr = fromMATFile(matFile)
             load(matFile,'map','motionTubes','pathLengths','roiPositions','trajectories');
             
